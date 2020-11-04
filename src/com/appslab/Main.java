ece76cd3240fc568b8f72d61b84e62d4f74f05e9
martinspilak <martinecko24@gmail.com>
2020-11-04 23:48:44 +0100

    1.Excercise

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,20 @@
 package com.appslab;
-
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(1);
+        arr.add(1);
+        arr.add(2);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        if (arr.isEmpty() == true)
+            System.out.println("The ArrayList is empty");
+        else
+            arr.forEach((n) -> System.out.println(n));
     }
 }
