9e1a012b1f295b7e1274ad82201bca9b2a3f0b5b
martinspilak <martinecko24@gmail.com>
2020-11-11 23:10:23 +0100

    Pokus č. 7184

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -5,16 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         array.addAll(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5));
-        addIfNotExist(6);
+        RemoveEven();
         System.out.println();
     }
-    public static void addIfNotExist(int number) {
-        if (array.contains(number))
-            array.forEach((n) -> System.out.println(n));
-        else{
-            array.add(number);
-            array.forEach(System.out::println);
-        }
-    }
+    public static void RemoveEven() {
+        array.stream().filter(a -> a % 2 != 0).forEach(System.out::println);
+            }
+}
 
-    }
+
+
+
+
