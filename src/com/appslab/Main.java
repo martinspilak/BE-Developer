45c8826516de60ba9db0c3b3fb0b57772b711290
martinspilak <martinecko24@gmail.com>
2020-11-17 00:23:47 +0100

    Pokus č. 57

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,19 +3,17 @@ import java.lang.reflect.Array;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-
-        Person[] obj = new Person[]{
-                new Person("John", 21, 23000),
-                new Person("Steve", 32, 40000),
-                new Person("Martin", 16, 2700)
-        };
-        System.out.println(PeoplesBudget(obj));
-    }
-    public static boolean PeoplesBudget(Person[] people){
-        return Arrays.stream(people).map(Person::getName).anyMatch(a -> a.contains("a"));
-            }
+        List<Integer> array = Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5);
+        HashSet<Integer> hashset = new HashSet<Integer>(array);
+        hashset.add(1);
+        hashset.forEach(System.out::println);
 }
-// Output: true
-
+}
+/* Output: 1
+           2
+           3
+           4
+           5
+*/
 
 
