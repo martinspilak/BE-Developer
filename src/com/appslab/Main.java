ed3fe41516748c706f31c0657c37069684f6bfdb
martinspilak <martinecko24@gmail.com>
2020-11-22 19:50:57 +0100

    22112020

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -8,10 +8,13 @@ public class Main {
       colors.put("Green", 2);
       colors.put("Black", 3);
       colors.put("White", 4);
-      System.out.println(colors);
+      System.out.println("HashMap: " + colors);
+      colors.putIfAbsent("Blue", 5);
+      System.out.println("New HashMap: " + colors);
 }
 }
-// Output: {Red=1, White=4, Black=3, Green=2}
-
+/* Output: HashMap: {Red=1, White=4, Black=3, Green=2}
+           New HashMap: {Red=1, White=4, Blue=5, Black=3, Green=2}
+*/
 
 
