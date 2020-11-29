7aa690b29b44e402b886291d2bec25d59104599b
martinspilak <martinecko24@gmail.com>
2020-11-29 22:42:42 +0100

    1101111000011011011010100

--- src/com/appslab/Programmer.java
+++ src/com/appslab/Programmer.java
@@ -5,7 +5,4 @@ public class Programmer extends JobType   {
     public Programmer(float salary, int bonus){
         super(salary, bonus, Employee.PROGRAMMER);
     }
-    public void getInfo() {
-        System.out.println(employee.job + "'s" + " salary is " + (salary + (float)bonus) + ".");
-}
 }
